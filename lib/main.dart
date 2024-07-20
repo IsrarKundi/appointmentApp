bfeec7e2250dd877ddcb0d2f4247af1e35a36379
israrKundi <israrikhere@gmail.com>
2024-07-21 00:06:46 +0500

    firebase connected

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,20 @@
 import 'package:appointment/screens/book_appointment.dart';
 import 'package:appointment/screens/home_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: FirebaseOptions(
+      apiKey: 'AIzaSyAlSEYcGyhGUFtz8iaSCnAiwCCxc_VrI_Q',
+      appId: '1:114539683397:android:14704311c3a53c0c0dcd2c',
+      messagingSenderId: '114539683397',
+      projectId: 'salon-appointment-b7091',
+      storageBucket: 'salon-appointment-b7091.appspot.com',
+    ),
+  );
   runApp(const MyApp());
 }
 
